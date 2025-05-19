fdc511e5cd71e15de14a313cfba114b3cec2d336
Dan-GH-str <danzhuff80603@mail.ru>
2025-05-19 22:25:34 +0300

    adaptability improved

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./Components/AppRouter";
 import "./styles/App.css"
 import { AuthProvider } from "./Context/AuthContext";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    function setVhProperty() {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    // Устанавливаем переменную при загрузке
+    setVhProperty()
+
+    // Добавляем обработчик resize
+    window.addEventListener('resize', setVhProperty)
+
+    // Чистим обработчик при размонтировании
+    return () => {
+      window.removeEventListener('resize', setVhProperty)
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <AuthProvider>
