b1a0e6d03838b70a618f121fca8b95967afb1b7e
Dan-GH-str <danzhuff80603@mail.ru>
2025-05-25 11:01:46 +0300

    adaptability improved

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./Components/AppRouter";
 import "./styles/App.css"
 import { AuthProvider } from "./Context/AuthContext";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    function setVhProperty() {
-      const vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', `${vh}px`)
-    }
+  // useEffect(() => {
+  //   // Функция для установки значения переменной --vh, чтобы высота экрана не уменьшалась при открытии клавиатуры с телефонов
+  //   function setVhProperty() {
+  //     const vh = window.innerHeight * 0.01
+  //     document.documentElement.style.setProperty('--vh', `${vh}px`)
+  //   }
 
-    // Устанавливаем переменную при загрузке
-    setVhProperty()
-
-    // // Добавляем обработчик resize
-    // window.addEventListener('resize', setVhProperty)
-
-    // // Чистим обработчик при размонтировании
-    // return () => {
-    //   window.removeEventListener('resize', setVhProperty)
-    // }
-  }, [])
+  //   // Устанавливаем переменную при загрузке
+  //   setVhProperty()
+  // }, [])
 
   return (
     <BrowserRouter>
